24429c5ed8c027b1114537c9174c6a56efab88dc
cheer4u <>
2025-09-20 10:30:22 +0900

    changed to use st.navigation

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import streamlit.components.v1 as components
 
 parser = argparse.ArgumentParser(description='Streamlit 3js')
 
-parser.add_argument('--html', default='earth.html', help='html file')
-args = parser.parse_args()
-
 st.set_page_config(
     page_title='Streamlit 3js',
     page_icon=None,
@@ -14,22 +11,32 @@ st.set_page_config(
     initial_sidebar_state='auto',
 )
 
-st.html(
-    '''
-    <style>
-        .block-container {
-            padding: 0 !important;
-            margin: 0 !important;
-            max-width: 100% !important;
-            width: 100% !important;
-        }
-        iframe.stIFrame {
-            width: 100vw;
-            height: 100vh;
-            display: block;
-        }
-    </style>
-    ''');
-with open(args.html, 'r') as f:
-    html = f.read()
-components.html(html, width=0, height=0)
+def threejs(app):
+    st.html(
+        '''
+        <style>
+            .block-container {
+                padding: 0 !important;
+                margin: 0 !important;
+                max-width: 100% !important;
+                width: 100% !important;
+            }
+            iframe.stIFrame {
+                width: 100vw;
+                height: 100vh;
+                display: block;
+            }
+        </style>
+        ''');
+    with open(app, 'r') as f:
+        html = f.read()
+    components.html(html, width=0, height=0)
+
+def threejs_earth():
+    threejs('earth.html')
+
+def threejs_grid():
+    threejs('grid.html')
+
+pg = st.navigation([threejs_earth, threejs_grid])
+pg.run()
